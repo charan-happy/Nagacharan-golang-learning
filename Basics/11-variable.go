35f2773c74bbaa4ad6917d717c2b65c9c95acb25
Nagacharan <gudiyathamx.nagacharan@intel.com>
2025-02-17 23:55:44 +0530

    day-2 of go bootcamp learning

---
+++ Basics/11-variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+/* output
+
+0 false false false
+
+*/
---
+++ Basics/12-variable-initializer.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, "charan", "no!"
+	fmt.Println(i, j, c, python, java)
+}
+
+/* output
+
+1 2 true charan no!
+
+*/
